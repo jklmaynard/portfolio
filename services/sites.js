a8070e4ed1bd5572f087680e79a07bd93fdb01c5
James Maynard <jameskirkmaynard@gmail.com>
2017-04-11 15:01:10 -0700

    build out site partials, skeleton styles

--- services/sites.js
+++ services/sites.js
@@ -3,12 +3,20 @@ angular.module('portfolio')
   var obj = {
     sites: [
       {
-        name: "Mother Foucault's",
+        name: "Mother Foucault's Bookshop",
         type: "Angular with Rails",
         website_url: "http://www.motherfoucaultsbookshop.com/",
         github_url: "https://github.com/jklmaynard/mother-foucaults",
         image: "img/mother-foucaults.jpg",
-        tile_background_color: ""
+        details: [
+          {text: "Provide single page application to display events and calendar"},
+          {text: "Installed calendar-ui dependencies with angular front end capabilities"},
+          {text: "Wrote app-specific domNode to create elements out of back-end data string"},
+          {text: "Implemented photo upload with Paperclip gem and base64 bower dependencies"},
+        ],
+        improvements: [
+          {text: "Could always use better style and displays"}
+        ]
       },
       {
         name: "Tavern Books",
@@ -16,7 +24,12 @@ angular.module('portfolio')
         website_url: "http://www.tavernbooks.org",
         github_url: "https://github.com/ceciliaxyang/tavern-books",
         image: "img/tavernbooks.jpg",
-        tile_background_color: ""
+        details: [
+          {text: ""}
+        ],
+        improvements: [
+          {text: ""}
+        ]
       },
       {
         name: "Poetry Website",
@@ -24,7 +37,12 @@ angular.module('portfolio')
         website_url: "http://www.jamesmaynardpoetry.com",
         github_url: "https://github.com/jklmaynard/poetry_site",
         image: "img/jamesmaynardpoetry.jpg",
-        tile_background_color: ""
+        details: [
+          {text: ""}
+        ],
+        improvements: [
+          {text: ""}
+        ]
       },
       {
         name: "Fun with Numbers",
@@ -32,7 +50,12 @@ angular.module('portfolio')
         website_url: "https://jklmaynard.github.io/fun-with-numbers",
         github_url: "https://github.com/jklmaynard/fun-with-numbers",
         image: "img/funwithnumbers.jpg",
-        tile_background_color: ""
+        details: [
+          {text: ""}
+        ],
+        improvements: [
+          {text: ""}
+        ]
       },
       {
         name: "Trinity United Methodist Church",
@@ -40,7 +63,12 @@ angular.module('portfolio')
         website_url: "http://warsawtrinityumc.com",
         github_url: "https://github.com/jklmaynard/trinity-church",
         image: "img/trinityunitedmethodist.jpg",
-        tile_background_color: ""
+        details: [
+          {text: ""}
+        ],
+        improvements: [
+          {text: ""}
+        ]
       },
       {
         name: "Book List",
@@ -48,7 +76,12 @@ angular.module('portfolio')
         website_url: "https://fast-atoll-17293.herokuapp.com/",
         github_url: "https://github.com/jklmaynard/book-list",
         image: "img/booklist.jpg",
-        tile_background_color: ""
+        details: [
+          {text: ""}
+        ],
+        improvements: [
+          {text: ""}
+        ]
       },
       {
         name: "This Portfolio",
@@ -56,7 +89,12 @@ angular.module('portfolio')
         website_url: "https://jklmaynard.github.io/portfolio/",
         github_url: "https://jklmaynard.github.io/portfolio/",
         image: "img/portfolio.png",
-        tile_background_color: ""
+        details: [
+          {text: ""}
+        ],
+        improvements: [
+          {text: ""}
+        ]
       },
     ]
   }
